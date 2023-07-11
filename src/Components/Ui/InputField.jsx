54dac4fb5c3934f1a6096ba93fab6276f7b22b45
NikolayB7 <biletskiyna@gmail.com>
2023-07-11 23:00:04 +0300

    calculate field

--- src/Components/Ui/InputField.jsx
+++ src/Components/Ui/InputField.jsx
@@ -1,30 +1,50 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { useSelector, useDispatch } from "react-redux";
-import { selectedToCurrency, setResultValue } from "../../store/choiseSlice"
+import { setResultValue, setSearch } from "../../store/choiseSlice"
 
-const InputField = ({ outField }) => {
+const InputField = ({ typeField, outField }) => {
     const currencyResult = useSelector(state => state.selectedCurrency.resultValue)
     const currencyFrom = useSelector(state => state.selectedCurrency.selectedFrom)
+    const currencyTo = useSelector(state => state.selectedCurrency.selectedTo)
     const currencyToVal = useSelector(state => state.selectedCurrency.selectedToVal)
     const dispatch = useDispatch()
 
-    const [fromValue, setFromValue] = useState('')
+    const [fromValue, setFromValue] = useState(0)
     const [toValue, setToValue] = useState(currencyResult)
 
     useEffect(() => {
         setToValue(currencyResult)
     }, [currencyResult])
+    useEffect(() => {
+        setFromValue(0)
+        setToValue(0)
+    }, [currencyTo, currencyFrom])
 
     const calculatePrice = (e) => {
-        let inputValue = Number(e.target.value),
-            result;
-        setFromValue(inputValue)
-        if (currencyFrom.cc === 'UAN') {
-            result = inputValue / currencyToVal
-        } else {
-            result = (inputValue / currencyFrom.rate) * currencyToVal
+        setFromValue(0)
+        let inputValue = e.target.value, result;
+
+        if (inputValue.startsWith('0') && inputValue.length > 1) {
+            inputValue = inputValue.slice(1);
         }
+        setFromValue(inputValue);
+        (currencyFrom.cc === 'UAH') ? result = inputValue / currencyToVal : result = (inputValue / currencyFrom.rate) * currencyToVal
         dispatch(setResultValue(result));
+    }
+
+    const findCurrency = (e) => {
+        let str = e.target.value
+        setFromValue(str)
+        dispatch(setSearch(str))
+        console.log(str);
+    }
+
+    const changeField = (e) => {
+        if (typeField === 'number') {
+            calculatePrice(e)
+        } else {
+            findCurrency(e)
+        }
 
     }
 
@@ -33,9 +53,11 @@ const InputField = ({ outField }) => {
             disabled={!outField}
             value={outField ? fromValue : toValue}
             className='field__control'
-            type="number"
-            onChange={(e) => calculatePrice(e)} />
+            type={typeField}
+            onChange={(e) => changeField(e)} />
     );
 };
-
+InputField.defaultProps = {
+    typeField: 'number'
+}
 export default InputField;
